256a28778e90d2c0eb08cd91ff66a667e4bd692a
Benjamin Evans <liridatdt@protonmail.com>
2025-10-22 12:19:34 +0100

    Adds online data for comparison (similar to ours) & read for it

---
+++ OnlineCarSafetyExplore.R
@@ -0,0 +1,16 @@
+library(readr)
+library(dplyr)
+library(purrr)
+library(stringr)
+
+# ---- Paths (adjust if needed) ----
+data_path  <- "automobile/imports-85.data"
+names_path <- "automobile/imports-85.names"  # optional: to peek at the info file
+
+auto <- read_csv(
+  file      = data_path,
+  col_names = names_path,
+  na = c("?", "NA")
+)
+
+summary(auto)
